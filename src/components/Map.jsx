ac3b00c90b78676f6ce1ecaf44837decf97a1219
Filip Filipovic <102156199+RaptorFF@users.noreply.github.com>
2025-11-21 13:13:44 +0100

    Add files via upload

---
+++ src/components/Map.jsx
@@ -0,0 +1,11 @@
+import styles from "./Map.module.css";
+
+function Map() {
+  return (
+    <div className={styles.mapContainer}>
+      <div className={styles.map}></div>
+    </div>
+  );
+}
+
+export default Map;
