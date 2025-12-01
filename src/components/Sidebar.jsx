daa04abea44dc6627188c157a80ae2273c1a604e
Filip Filipovic <102156199+RaptorFF@users.noreply.github.com>
2025-12-01 15:16:16 +0100

    Add copyright footer to Sidebar component
    Added copyright footer to the Sidebar component.

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,5 @@
 import { Outlet } from "react-router-dom";
 import AppNav from "./AppNav";
-import Footer from "./Footer";
 import Logo from "./Logo";
 import styles from "./Sidebar.module.css";
 
@@ -12,7 +11,11 @@ function Sidebar() {
 
       <Outlet />
 
-      <Footer />
+      <footer className={styles.footer}>
+        <p className={styles.copyright}>
+          &copy; Copyright {new Date().getFullYear()} by WorldWise Inc.
+        </p>
+      </footer>
     </div>
   );
 }
