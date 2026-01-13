dd0de075b2cd43d0a2830b4833efb43d09dd0d50
RaptorFF <f.filipovic84@gmail.com>
2026-01-13 11:21:13 +0100

    Initial commit

---
+++ src/components/CityItem.jsx
@@ -0,0 +1,54 @@
+import { Link } from "react-router-dom";
+import { useCities } from "../contexts/CitiesContext";
+import styles from "./CityItem.module.css";
+
+const formatDate = (date) =>
+  new Intl.DateTimeFormat("en", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  }).format(new Date(date));
+
+function CityItem({ city }) {
+  // If You face a problem showing the countries flags (emoji is not working)
+  // Here is the most simple solution I found (at least in my opinion)😍
+  // paste this function in the CityItem.jsx 👇
+
+  const flagemojiToPNG = (flag) => {
+    var countryCode = Array.from(flag, (codeUnit) => codeUnit.codePointAt())
+      .map((char) => String.fromCharCode(char - 127397).toLowerCase())
+      .join("");
+    return (
+      <img src={`https://flagcdn.com/24x18/${countryCode}.png`} alt="flag" />
+    );
+  };
+
+  const { currentCity, deleteCity } = useCities();
+  const { cityName, emoji, date, id, position } = city;
+
+  function handleClick(e) {
+    e.preventDefault();
+    deleteCity(id);
+  }
+
+  return (
+    <li>
+      <Link
+        className={`${styles.cityItem} ${
+          id === currentCity.id ? styles["cityItem--active"] : ""
+        }`}
+        to={`${id}?lat=${position.lat}&lng=${position.lng}`}
+      >
+        {/*  then write in the emoji part as this */}
+        <span className={styles.emoji}>{flagemojiToPNG(emoji)}</span>
+        <h3 className={styles.name}>{cityName}</h3>
+        <time className={styles.date}>({formatDate(date)})</time>
+        <button className={styles.deleteBtn} onClick={handleClick}>
+          &times;
+        </button>
+      </Link>
+    </li>
+  );
+}
+
+export default CityItem;
